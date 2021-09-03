b5536eb9248d640ad9d79430083582ee01139bef
macbew <62144432+macbew@users.noreply.github.com>
2021-09-03 17:36:54 +0300

    feat:  implement GuessingGame class

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min
+        this.max
+        this.candidate
+    }
 
     setRange(min, max) {
-
+        this.min = min
+        this.max = max
     }
 
     guess() {
-
+        return this.candidate = Math.ceil((this.min + this.max) / 2)
     }
 
     lower() {
-
+        this.max = this.candidate
     }
 
     greater() {
-
+        this.min = this.candidate
     }
 }
 
